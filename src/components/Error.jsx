8031d59003a43a91b2369cb474f5847e1c574af0
Blessing Abobo <suspieabobo@yahoo.com>
2019-08-02 11:51:06 +0100

    Buidt an error page and Add readme

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div className="error">
-      <i className="fa fa-heartbeat fa-2x text-center" />
+    <div className="col-8 error">
+      <div className="text-center">
+        <i className="fa fa-heartbeat fa-3x" />
+      </div>
       <h1 className="text-center">Something Went wrong</h1>
     </div>
   );
